bc158cd17356df8c8e475a953b3fa684afc91ee0
Baptiste Fouques <bateast@bat.fr.eu.org>
2024-04-23 16:38:33 +0200

    First try to have a basic addon

--- default.py
+++ default.py
@@ -13,6 +13,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
+DEFAULT_MANIFESTATION=0
+
 def build_url(query):
     base_url = sys.argv[0]
     return base_url + '?' + urllib.urlencode(query)
@@ -25,68 +27,56 @@ def parse_json(data):
             m[k] = parsed[k]
         return m[k]
 
-    def expand_element(element):
-        return expand_element(parsed[element]) if isinstance(element, int) and not isinstance(parsed[element], int) else \
-            expand_dict(element) if isinstance(element, dict) else \
-            expand_tuple(element) if isinstance(element, tuple) else \
-            parsed[element] if isinstance(element, int) else \
-            element
+
+    def expand_element(e):
+            match e :
+                case dict() :
+                    return expand_dict(e)
+                case tuple() :
+                    return expand_tuple(e)
+                case list() :
+                    return expand_tuple(e)
+                case int() if e < len(parsed) and not isinstance(parsed[e], int):
+                    return expand_element(parsed[e])
+                case _:
+                    return e
 
     def expand_tuple(element):
-        return map(lambda v: expand_element(v), element)
+        return [expand_element(v) for v in element]
 
     def expand_dict(element):
         return {k: expand_element(v) for k,v in element.items()}
 
-
     models={}
-    for element in parsed:
-        if isinstance(element, dict) and "model" in element:
-            model = get_or_add(models, element["model"])
-            print(expand_element(element))
+    elements=[]
 
-    return parsed
+    for e in parsed:
+        if isinstance(e, dict) and "model" in e and get_or_add(models, e["model"]) == "Expression" :
+            elements += [expand_element(e)]
+    return elements
 
-def parse_page(page):
-    songs = {}
-    index = 1
-    # the sample below is specific for the page we are scraping
-    # you will need to view the source of the page(s) you are
-    # planning to scrape to find the content you want to display
-    # this will return all the <a> elements on the page:
-    # <a href="some_url">some_text</a>
-    for item in page.find_all('a'):
-        # the item contains a link to an album cover
-        if item['href'].find('.jpg') > 1:
-            # format the url for the album cover to include the site url and url encode any spaces
-            album_cover = '{0}{1}'.format(sample_page, item['href'].replace(' ', '%20'))
-        # the item contains a link to a song containing '.mp3'
-        if item['href'].find('.mp3') > 1:
-            # update dictionary with the album cover url, song filename, and song url
-            songs.update({index: {'album_cover': album_cover, 'title': item['href'], 'url': '{0}{1}'.format(sample_page, item['href'])}})
-            index += 1
-    return songs
-    
-def build_song_list(songs):
+def build_song_list(content):
+    from time import strftime, localtime
+
     song_list = []
     # iterate over the contents of the dictionary songs to build the list
-    for song in songs:
+    for item in [e for e in content if e["model"] == "Expression"]:
         # create a list item using the song filename for the label
-        li = xbmcgui.ListItem(label=songs[song]['title'], thumbnailImage=songs[song]['album_cover'])
-        # set the fanart to the albumc cover
-        li.setProperty('fanart_image', songs[song]['album_cover'])
-        # set the list item to playable
+        li = xbmcgui.ListItem(label=item['title'], thumbnailImage=item['visual']['src'])
+        li.setProperty('inputstream', item['manifestations'][DEFAULT_MANIFESTATION]['url'])
+        li.setInfo('music', {'duration': item['manifestations'][DEFAULT_MANIFESTATION]['duration'],
+                             'date': strftime('%d.%m.%y', localtime(item['publishedDate']))
+                             })
         li.setProperty('IsPlayable', 'true')
         # build the plugin url for Kodi
         # Example: plugin://plugin.audio.example/?url=http%3A%2F%2Fwww.theaudiodb.com%2Ftestfiles%2F01-pablo_perez-your_ad_here.mp3&mode=stream&title=01-pablo_perez-your_ad_here.mp3
-        url = build_url({'mode': 'stream', 'url': songs[song]['url'], 'title': songs[song]['title']})
+        url = build_url({'mode': 'stream', 'url': item['manifestations'][DEFAULT_MANIFESTATION]['url'], 'title': item['title']})
         # add the current list item to a list
-        song_list.append((url, li, False))
+        song_list.append((url, li, False)) # False if not a folder
     # add list to Kodi per Martijn
     # http://forum.kodi.tv/showthread.php?tid=209948&pid=2094170#pid2094170
     xbmcplugin.addDirectoryItems(addon_handle, song_list, len(song_list))
     # set the content of the directory
-    xbmcplugin.setContent(addon_handle, 'songs')
     xbmcplugin.endOfDirectory(addon_handle)
 
 def play_song(url):
@@ -107,7 +97,7 @@ def main():
         page = requests.get(radiofrance_page).text
         # get the content needed from the page
         content = parse_json(page)
-        print(content)
+        print (content)
 
         # display the list of songs in Kodi
         build_song_list(content)
