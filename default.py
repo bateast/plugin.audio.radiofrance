7101f809856293e7bf43a4514ea610d69e4b0fa2
Baptiste Fouques <bateast@bat.fr.eu.org>
2024-05-13 21:18:05 +0200

    Reorganise code and managebrand direct radio

--- default.py
+++ default.py
@@ -1,8 +1,9 @@
-# https://docs.python.org/2.7/
 import json
 import sys
 import requests
 from urllib.parse import parse_qs
+from enum import Enum
+
 # http://mirrors.kodi.tv/docs/python-docs/
 # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 from urllib.parse import urlencode, quote_plus
@@ -11,222 +12,140 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 
+from utils import Model, Item, Brand, expand_json, build_url
+
 
 DEFAULT_MANIFESTATION = 0
 RADIOFRANCE_PAGE = "https://www.radiofrance.fr"
 
 
-def build_url(query):
-    base_url = sys.argv[0]
-    url = base_url + '?' + urlencode(query, quote_via=quote_plus)
-    return url
-
-
-def parse_json(data):
-    parsed = json.loads(data)["nodes"][-1]["data"]
-
-    def get_or_add(m, k):
-        if k not in m:
-            m[k] = parsed[k]
-        return m[k]
-
-    def expand_element(e):
-        if isinstance(e, dict):
-            return expand_dict(e)
-        elif isinstance(e, tuple):
-            return expand_tuple(e)
-        elif isinstance(e, list):
-            return expand_tuple(e)
-        a = parsed[e]
-        if isinstance(a, dict):
-            return expand_dict(a)
-        elif isinstance(a, tuple):
-            return expand_tuple(a)
-        elif isinstance(a, list):
-            return expand_tuple(a)
-        return a
-
-    def expand_tuple(element):
-        return [expand_element(v) for v in element]
-
-    def expand_dict(element):
-        return {k: expand_element(v) for k, v in element.items()}
-
-    return expand_element(parsed[0])
-
-
-def build_lists(data, args):
+def build_lists(data, args, url):
     xbmc.log(str(args), xbmc.LOGINFO)
+
+    def add_search():
+        new_args = {k: v[0] for (k, v) in args.items()}
+        new_args["mode"] = "search"
+        li = xbmcgui.ListItem(label="? Search term")
+        li.setIsFolder(True)
+        new_url = build_url(new_args)
+        highlight_list.append((new_url, li, True))
+
+    def add_pages(item):
+        new_args = {k: v[0] for (k, v) in args.items()}
+        pages = item.pages
+        if 1 < pages["pageNumber"]:
+            new_args["page"] = pages["pageNumber"] - 1
+            li = xbmcgui.ListItem(label="<< Previous page")
+            li.setIsFolder(True)
+            new_url = build_url(new_args)
+            highlight_list.append((new_url, li, True))
+        if pages["pageNumber"] < pages["lastPage"]:
+            new_args["page"] = pages["pageNumber"] + 1
+            li = xbmcgui.ListItem(label=">> Next page")
+            li.setIsFolder(True)
+            new_url = build_url(new_args)
+            highlight_list.append((new_url, li, True))
 
     highlight_list = []
     song_list = []
 
-    def add_search():
-        new_args = {k: v[0] for (k, v) in args.items()}
-        new_args['mode'] = "search"
-        li = xbmcgui.ListItem(label="? Search term")
-        li.setIsFolder(True)
-        url = build_url(new_args)
-        highlight_list.append((url, li, True))
-
-    def add_pages(pages):
-        new_args = {k: v[0] for (k, v) in args.items()}
-        if 1 < pages['pageNumber']:
-            new_args['page'] = pages['pageNumber'] - 1
-            li = xbmcgui.ListItem(label="<< Previous page")
-            li.setIsFolder(True)
-            url = build_url(new_args)
-            highlight_list.append((url, li, True))
-        if pages['pageNumber'] < pages['lastPage']:
-            new_args['page'] = pages['pageNumber'] + 1
-            li = xbmcgui.ListItem(label=">> Next page")
-            li.setIsFolder(True)
-            url = build_url(new_args)
-            highlight_list.append((url, li, True))
-
-    def setArt(li, item):
-        if 'visual' in item and\
-           item['visual'] is not None and\
-           'src' in item['visual']:
-            thumb = item['visual']['src']
-        elif 'mainImage' in item and\
-           item['mainImage'] is not None and\
-           'src' in item['mainImage']:
-            thumb = item['mainImage']['src']
-        else:
-            thumb = None
-        if 'squaredVisual' in item and\
-           item['squaredVisual'] is not None and\
-           'src' in item['squaredVisual']:
-            icon = item['squaredVisual']['src']
-        else:
-            icon = None
-        li.setArt({'thumb': thumb,
-                   'icon': icon})
-
-    def getPath(li, item, args, index=0):
-        if 'path' in item and item['path'] is not None:
-            return build_url({'mode': 'page', 'title': item['title'],
-                             'url': RADIOFRANCE_PAGE + "/" + item['path']})
-        elif 'links' in item and item['links'] is not None \
-             and 0 < len([l for l in item['links'] if l['type'] == "path"]):
-            # Warning, Links elements seems relative to current brand url
-            link = [l for l in item['links'] if l['type'] == "path"][0]
-            brand = item['brand'] if 'brand' in item and not "" else ""
-            return build_url({'mode': 'page', 'title': item['title'],
-                             'url': RADIOFRANCE_PAGE + "/" + brand + "/" + link['path']})
-
-        else:
-            # Add element for further development
-            return build_url({'mode': 'highlight',
-                              'title': item['title'],
-                              'url': args['url'][0] if 'url' in args
-                              else RADIOFRANCE_PAGE + "/podcasts",
-                              'index': index})
-
-    def add_item(item, index=0):
-        xbmc.log(str(item['title']) + " - " + str(item['model']), xbmc.LOGINFO)
-
-        def match_folder():
-            li = xbmcgui.ListItem(label=item['title'])
-            li.setIsFolder(True)
-            setArt(li, item)
-            url = getPath(li, item, args, index)
-            highlight_list.append((url, li, True))
-
-        def match_song():
-            from time import strftime, localtime
-            li = xbmcgui.ListItem(label=item['title'])
-            tag = li.getMusicInfoTag(offscreen=True)
-            tag.setMediaType("audio")
-            tag.setTitle(item['title'])
-            tag.setComment(item['standFirst'] if 'standFirst' in item else None)
-            tag.setURL(item['manifestations'][DEFAULT_MANIFESTATION]['url'])
-            tag.setGenres(["podcast"])
-            if 'guest' in item:
-                tag.setArtist(", ".join([g['name'] for g in item['guest']]))
-            tag.setDuration(
-                item['manifestations'][DEFAULT_MANIFESTATION]['duration'])
-            tag.setReleaseDate(
-                strftime('%d-%m.%y', localtime(item['publishedDate'])) if 'publishedDate' in item else None)
-            setArt(li, item)
-            li.setProperty('IsPlayable', 'true')
-            out_url = item['manifestations'][DEFAULT_MANIFESTATION]['url']
-            url = build_url({'mode': 'stream',
-                             'url': out_url,
-                             'title': item['title']})
-            song_list.append((url, li, False))
-
-        if item['model'] in ["Highlight", "Concept", "Tag",
-                             "Theme", "PageTemplate", 'HighlightElement']:
-            # If element is a singleton, derictly develop it
-            if 'elements' in item and len(item['elements']) == 1 \
-               and 'contents' in item['elements'][0] and len(item['elements'][0]['contents']) == 1:
-                add_item(item['elements'][0]['contents'][0], index)
-            elif 'elements' in item and len(item['elements']) == 1 \
-                 and 'contents' in item['elements'][0] and len(item['elements'][0]['contents']) == 0:
-                add_item(item['elements'][0], index)
-            else:
-                match_folder()
-        elif item['model'] in ["Expression"] and 0 < len(item['manifestations']):
-            match_song()
-
-    mode = args.get('mode', None)
+    mode = args.get("mode", [None])[0]
     if mode is None:
         add_search()
 
-    index = literal_eval(args['index'][0]) if 'index' in args else []
-    if 'podcastsData' in data:
-        data = data['podcastsData']
-        if 'pagination' in data:
-            data = data['pagination']
-        if 'pageNumber' in data and 'lastPage' in data\
-           and 1 < data['lastPage']:
-            add_pages(data)
-        if 'items' in data:
-            items = data['items']
-            for item in items:
-                add_item(item, index)
-
-    elif 'content' in data and 'layout' in data['content']:
-        items = data['content']['layout']
-        for (i, j) in index:
-            items = items['elements'][i]['contents'][j]
-        i = 0
-        for element in items['elements']:
-            j = 0
-            for content in element['contents']:
-                add_item(content, index=index + [(i, j)])
-                j += 1
-            i += 1
-    elif 'content' in data and 'expressions' in data['content']:
-        data = data['content']['expressions']
-        if 'pageNumber' in data and 'lastPage' in data\
-           and 1 < data['lastPage']:
-            add_pages(data)
-        if 'items' in data:
-            items = data['items']
-            for item in items:
-                add_item(item, index)
-    elif 'pagination' in data:
-        pagination = data['pagination']
-        if 'pageNumber' in pagination and 'lastPage' in pagination\
-           and 1 < pagination['lastPage']:
-            add_pages(pagination)
-        if 'items' in data:
-            items = data['items']
-            for (k, v) in items.items():
-                if k in ["concepts", "expressions_articles"]:
-                    for item in v['contents']:
-                        add_item(item, index)
+    item = Item(data["podcastsData"] if "podcastsData" in data else data["content"] if "content" in data else data["layout"] if "layout" in data else data)
+    if mode == "index":
+        element_index = int(args.get("index", [None])[0])
+        list = Item(item.subs[element_index]).elements
     else:
-        None
+        list = item.subs
+        xbmc.log( str(mode), xbmc.LOGINFO)
 
-    xbmcplugin.addDirectoryItems(
-        addon_handle, highlight_list, len(highlight_list))
+    index = 0
+    for data in list:
+        sub_item = Item(data)
+        while (
+            sub_item.is_folder() and len(sub_item.subs) == 1 and sub_item.path is None
+        ):
+            sub_item = Item(sub_item.subs[0])
+
+        # Create kodi element
+        if sub_item.is_folder():
+            if sub_item.path is not None:
+                li = xbmcgui.ListItem(label="🗁 " + sub_item.title)
+                li.setArt({"thumb": sub_item.image, "icon": sub_item.icon})
+                li.setIsFolder(True)
+                new_args = {"title": "🗁 " + sub_item.title}
+                new_args["url"] = sub_item.path if sub_item.model == Model["Brand"] else RADIOFRANCE_PAGE + "/" + sub_item.path
+                new_args["mode"] = "brand" if sub_item.model == Model["Brand"] else "url"
+                builded_url = build_url(new_args)
+                highlight_list.append((builded_url, li, True))
+            if 0 < len(sub_item.elements) :
+                li = xbmcgui.ListItem(label="⭐ " + sub_item.title)
+                li.setArt({"thumb": sub_item.image, "icon": sub_item.icon})
+                li.setIsFolder(True)
+                new_args = {"title": "⭐ " + sub_item.title}
+                new_args["url"] = url
+                new_args["index"] = index
+                new_args["mode"] = "index"
+                builded_url = build_url(new_args)
+                highlight_list.append((builded_url, li, True))
+        else:
+            # Playable element
+            li = xbmcgui.ListItem(label=sub_item.title)
+            li.setArt({"thumb": sub_item.image, "icon": sub_item.icon})
+            new_args = {"title": sub_item.title}
+            li.setIsFolder(False)
+            tag = li.getMusicInfoTag(offscreen=True)
+            tag.setMediaType("audio")
+            tag.setTitle(sub_item.title)
+            # tag.setComment(sub_item.standFirst if "standFirst" in sub_item else None)
+            tag.setURL(sub_item.path)
+            tag.setGenres(["podcast"])
+            tag.setArtist(sub_item.artists)
+            tag.setDuration(sub_item.duration)
+            tag.setReleaseDate(sub_item.release)
+            li.setProperty("IsPlayable", "true")
+            if sub_item.path is not None:
+                new_args["url"] = sub_item.path
+                new_args["mode"] = "stream"
+            builded_url = build_url(new_args)
+            song_list.append((builded_url, li, False))
+
+        xbmc.log(
+            str(new_args) + str(sub_item),
+            xbmc.LOGINFO,
+        )
+        index += 1
+
+    xbmcplugin.addDirectoryItems(addon_handle, highlight_list, len(highlight_list))
     xbmcplugin.addDirectoryItems(addon_handle, song_list, len(song_list))
     xbmcplugin.endOfDirectory(addon_handle)
 
+def brand(args):
+    name = args.get("url", [""])[0]
+    url = RADIOFRANCE_PAGE + "/" + name.split("_")[0] + "/api/live/webradios/" + name
+
+    xbmc.log("[Brand]: " + url, xbmc.LOGINFO)
+
+    data = requests.get(url).text
+    item = Brand(data)
+
+    li = xbmcgui.ListItem(label=item.title)
+    li.setArt({"thumb": item.image, "icon": item.icon})
+    li.setIsFolder(False)
+    tag = li.getMusicInfoTag(offscreen=True)
+    tag.setMediaType("audio")
+    tag.setTitle(item.title)
+    tag.setURL(item.url)
+    tag.setGenres(["radio"])
+    # tag.setArtist(item.artists)
+    # tag.setDuration(item.duration)
+    # tag.setReleaseDate(sub_item.release)
+    li.setProperty("IsPlayable", "true")
+    
+    xbmc.Player().play(item.url, li)
+    play (item.url)
 
 def play(url):
     play_item = xbmcgui.ListItem(path=url)
@@ -235,7 +154,7 @@ def play(url):
 
 def search(args):
     def GUIEditExportName(name):
-        kb = xbmc.Keyboard('Odyssées', 'Search title')
+        kb = xbmc.Keyboard("Odyssées", "Search title")
         kb.doModal()
         if not kb.isConfirmed():
             return None
@@ -243,50 +162,60 @@ def search(args):
         return query
 
     new_args = {k: v[0] for (k, v) in args.items()}
-    new_args['mode'] = "page"
+    new_args["mode"] = "page"
     value = GUIEditExportName("Odyssées")
     if value is None:
         return
 
-    new_args['url'] = RADIOFRANCE_PAGE + "/recherche"
+    new_args["url"] = RADIOFRANCE_PAGE + "/recherche"
     new_args = {k: [v] for (k, v) in new_args.items()}
     build_url(new_args)
     get_and_build_lists(new_args, url_args="?term=" + value + "&")
 
 
 def get_and_build_lists(args, url_args="?"):
-    xbmc.log("".join(["Get and build: " + str(args) + "(url args: " + url_args + ")"]), xbmc.LOGINFO)
-    url = args['url'][0]
+    xbmc.log(
+        "".join(["Get and build: " + str(args) + "(url args: " + url_args + ")"]),
+        xbmc.LOGINFO,
+    )
+    url = args.get("url", [RADIOFRANCE_PAGE])[0]
     page = requests.get(url + "/__data.json" + url_args).text
-    content = parse_json(page)
+    content = expand_json(page)
 
-    build_lists(content, args)
+    build_lists(content, args, url)
 
 
 def main():
     args = parse_qs(sys.argv[2][1:])
-    mode = args.get('mode', None)
+    mode = args.get("mode", None)
 
-    xbmc.log("".join(["mode: ", str("" if mode is None else mode[0]), ", args: ", str(args)]), xbmc.LOGINFO)
+    xbmc.log(
+        "".join(
+            ["mode: ", str("" if mode is None else mode[0]), ", args: ", str(args)]
+        ),
+        xbmc.LOGINFO,
+    )
 
     # initial launch of add-on
     url = RADIOFRANCE_PAGE
     url_args = "?"
     url_args += "recent=false&"
-    if 'page' in args and 1 < int(args['page'][0]):
-        url_args += "p=" + str(args['page'][0])
-    if mode is None:
-        url = url + "/podcasts"
-        page = requests.get(url + "/__data.json" + url_args).text
-        content = parse_json(page)
-        build_lists(content, args)
-    elif mode[0] == 'stream':
-        play(args['url'][0])
-    elif mode[0] == 'search':
+    if "page" in args and 1 < int(arg.get("page", "1")):
+        url_args += "p=" + str(args.get("page", "1"))
+    if mode is not None and mode[0] == "stream":
+        play(args("url"))
+    elif mode is not None and mode[0] == "search":
         search(args)
+    elif mode is not None and mode[0] == "brand":
+        brand(args)
     else:
+        if mode is None:
+            url = url + "/podcasts"
+            args["url"] = []
+            args["url"].append(url)
+        # New page
         get_and_build_lists(args, url_args)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     addon_handle = int(sys.argv[1])
     main()
--- utils.py
+++ utils.py
@@ -1,7 +1,149 @@
 #!/bin/python3
 
+from urllib.parse import urlencode, quote_plus
 import json
 import sys
+from enum import Enum
+from time import localtime, strftime
+
+
+class Model(Enum):
+    Other = 0
+    Theme = 1
+    Concept = 2
+    Highlight = 3
+    HighlightElement = 4
+    Expression = 5
+    ManifestationAudio = 6
+    EmbedImage = 7
+    PageTemplate = 8
+    Brand = 9
+
+
+class Item:
+    def __init__(self, data):
+        # Model
+        self.model = Model[data["model"]] if "model" in data else Model["Other"]
+        # Path
+        self.path = (
+            data["manifestations"][0]["url"]
+            if self.model == Model["Expression"] and "manifestations" in data and data["manifestations"] != []
+            else data["path"]
+            if "path" in data and data["path"] is not None and data["path"] != ""
+            else data["links"][0]["path"]
+            if "links" in data and data["links"] != []
+            else data["slug"] if self.model == Model["Brand"]
+            else None
+        )
+        if not self.is_folder():
+            # Guests
+            self.artists = ", ".join([g["name"] for g in data["guest"]]) if "guest" in data else ""
+            # Duration
+            self.duration = int(data["manifestations"][0]["duration"]) if "manifestations" in data and data["manifestations"] != [] else 0
+            # Release
+            self.release = strftime("%d-%m.%y", localtime(data["publishedDate"])) if "publishedDate" in data else None
+        # Sub elements
+        if self.model == Model["Highlight"]:
+            self.subs = data["elements"]
+        elif "layout" in data:
+            self.subs = data["layout"]["elements"]
+        elif "elements" in data:
+            self.subs = data["elements"]
+        elif "contents" in data and 0 < len(data["contents"]):
+            self.subs = data["contents"]
+        elif "content" in data and "layout" in data["content"]:
+            self.subs = data["content"]["layout"]
+        elif "content" in data and "expressions" in data["content"]:
+            self.subs = data["content"]["expressions"]
+        elif "expressions" in data:
+            self.subs = data["expressions"]["items"]
+        elif "pagination" in data:
+            self.subs = data["pagination"]["items"]
+        else:
+            self.subs = []
+        # Remove singletons
+        self.elements = self.subs
+        while len(self.elements) == 1:
+            self.elements = Item(self.elements[0]).subs
+
+        # Image
+        self.image = None
+        for key in ["mainImage", "visual"]:
+            if key in data and data[key] is not None and "src" in data[key]:
+                self.image = data[key]["src"]
+        self.icon = None
+        for key in ["squaredVisual"]:
+            if key in data and data[key] is not None and "src" in data[key]:
+                self.icon = data[key]["src"]
+        # Other pages (tuple (x,n): current page x over n)
+        self.pages = (1, 1)
+        if "pageNumber" in data and "lastPage" in data:
+            self.pages = (data["pageNumber"], data["lastPage"])
+        elif "pagination" in data:
+            self.pages = (
+                data["pagination"]["pageNumber"],
+                data["pagination"]["lastPage"],
+            )
+
+        # Title
+        self.title = (
+            str(data["title"])
+            if "title" in data and data["title"] is not None
+            else self.subs[0]["title"]
+            if len(self.subs) == 1
+            else data["shortTitle"]
+            if "shortTitle" in data
+            else None
+        )
+
+    def __str__(self):
+        return (
+            str(self.pages)
+            if self.pages != (1, 1)
+            else ""
+            + str(self.title)
+            + " ["
+            + str(self.model)
+            + "]"
+            + " ["
+            + str(len(self.elements))
+            + "] ("
+            + str(self.path)
+            + ")"
+        )
+
+    def is_folder(self):
+        return self.model in [
+            Model["Theme"],
+            Model["Concept"],
+            Model["Highlight"],
+            Model["HighlightElement"],
+            Model["PageTemplate"],
+            Model["Brand"],
+        ]
+
+    def is_image(self):
+        return self.model in [Model["EmbedImage"]]
+
+    def is_audio(self):
+        return not self.is_folder() and not self.is_image()
+
+
+class Brand:
+
+    def __init__(self, page):
+        data = json.loads(page)
+        self.title = data["stationName"]
+        self.image = None
+        for key in ["mainImage", "visual"]:
+            if key in data and data[key] is not None and "src" in data[key]:
+                self.image = data[key]["src"]
+        self.icon = None
+        for key in ["squaredVisual"]:
+            if key in data and data[key] is not None and "src" in data[key]:
+                self.icon = data[key]["src"]
+        self.url = data["now"]["media"]["sources"][0]["url"]
+
 
 
 def expand_json(data):
@@ -32,6 +174,29 @@ def expand_json(data):
     return expand_element(parsed[0])
 
 
-if __name__ == '__main__':
+def build_url(query):
+    base_url = sys.argv[0]
+    url = base_url + "?" + urlencode(query, quote_via=quote_plus)
+    return url
+
+
+if __name__ == "__main__":
     data = sys.stdin.read()
-    print(json.dumps(expand_json(data)))
+    data = expand_json(data)
+    # print(json.dumps(expanded))
+
+    item = Item(data["podcastsData"] if "podcastsData" in data else data["content"] if "content" in data else data["layout"] if "layout" in data else data)
+    print(str(item))
+
+    if 1 < len(sys.argv):
+        index = int(sys.argv[1])
+        subs = Item(item.subs[index]).elements
+    else:
+        subs = item.subs
+
+    for data in subs:
+        sub_item = Item(data)
+        while sub_item.is_folder() and len(sub_item.subs) == 1 and sub_item.path is None:
+            sub_item = Item(sub_item.subs[0])
+        print(str(sub_item))
+
